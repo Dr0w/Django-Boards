056f83e205a4e82eb3bdc204d67bdb8730e73bf1
Dr0w <drow.public@gmail.com>
2022-10-19 21:29:07 -0400

    Adding new test

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from .views import home, board_topics
 from .models import Board
 
+
 class HomeTests(TestCase):
     def setUp(self):
         self.board = Board.objects.create(name='Django', description='Django board.')
@@ -19,6 +20,7 @@ class HomeTests(TestCase):
     def test_home_view_contains_link_to_topics_page(self):
         board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
         self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
+
 
 class BoardTopicsTests(TestCase):
     def setUp(self):
@@ -37,3 +39,9 @@ class BoardTopicsTests(TestCase):
     def test_board_topics_url_resolves_board_topics_view(self):
         view = resolve('/boards/1/')
         self.assertEquals(view.func, board_topics)
+
+    def test_board_topics_view_contains_link_back_to_homepage(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(board_topics_url)
+        homepage_url = reverse('home')
+        self.assertContains(response, 'href="{0}"'.format(homepage_url))
