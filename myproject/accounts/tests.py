4dcc9390fd827e2e9708734406cd72fd081e90e9
Dr0w <drow.public@gmail.com>
2022-11-07 21:52:13 -0500

    Added test for signup

--- myproject/accounts/tests.py
+++ myproject/accounts/tests.py
@@ -1,3 +1,15 @@
+from django.urls import reverse, resolve
 from django.test import TestCase
 
-# Create your tests here.
+from .views import signup
+
+
+class SignUpTests(TestCase):
+    def test_signup_status_code(self):
+        url = reverse('signup')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_signup_url_resolves_signup_view(self):
+        view = resolve('/signup/')
+        self.assertEquals(view.func, signup)
